762868d40e47166e4535971d28e3ea444cdaaf90
kamuiroeru <19339328+kamuiroeru@users.noreply.github.com>
2023-08-29 23:36:25 +0900

    Initial commit

---
+++ app.vue
@@ -0,0 +1,23 @@
+<template>
+  <UContainer class="mt-4">
+    <UCard>
+      <template #header>
+        <div class="m-0 p-0 flex items-center gap-2">
+          This is the header with a
+          <UButton icon="i-ic-baseline-alternate-email" variant="outline"
+            >button</UButton
+          >
+        </div>
+      </template>
+
+      <p>This is the body with a <UBadge>Badge</UBadge></p>
+      <p class="text-teal-500">
+        Hogefuga
+        <UButton
+          :ui="{ rounded: 'rounded-full', size: 'xl' }"
+          icon="i-heroicons-pencil-square"
+        />
+      </p>
+    </UCard>
+  </UContainer>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@nuxthq/ui'],
+  ui: {
+    icons: ['heroicons', 'mdi', 'ic'],
+  },
+});
